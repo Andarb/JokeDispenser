9515edbe300ea040b6f47b8b7088324fedd7417f
Andrius Narbuntas <tux.way@gmail.com>
2018-06-02 16:38:18 +0100

    Configure Google Cloud Endpoint to retrieve a joke

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -1,10 +1,10 @@
 package com.udacity.gradle.builditbigger.backend;
 
+import com.github.andarb.jokesource.JokeSource;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
 
-import javax.inject.Named;
 
 /** An endpoint class we are exposing */
 @Api(
@@ -18,11 +18,17 @@ import javax.inject.Named;
 )
 public class MyEndpoint {
 
-    /** A simple endpoint method that takes a name and says Hi back */
-    @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
+    /**
+     * A simple endpoint method that retrieves a joke from a Java library and passes it along
+     */
+    @ApiMethod(name = "getJoke")
+    public MyBean getJoke() {
+        // Retrieve a joke from our Java library
+        JokeSource jokeSource = new JokeSource();
+        String joke = jokeSource.retrieveJoke();
+
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
+        response.setData(joke);
 
         return response;
     }
